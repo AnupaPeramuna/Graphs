12258b60644fea9b9b228d5d8469b88c3ba1616b
AnupaPeramuna <140163292+AnupaPeramuna@users.noreply.github.com>
2026-05-28 09:44:02 +1000

    Add files via upload
    initial commit

---
+++ vega_lite_vis.js
@@ -0,0 +1,21 @@
+// var vg_1 = "Graph.vg.json";
+// vegaEmbed("#bar_chart", vg_1).then(function(result) {
+    
+// }).catch(console.error);
+
+var vg_2 = "StopsGraph.vg.json";
+vegaEmbed("#stop", vg_2).then(function(result) {
+    
+}).catch(console.error);
+
+
+
+var vg_3 = "servicesPer10k.vg.json";
+vegaEmbed("#services", vg_3).then(function(result) {
+    
+}).catch(console.error);
+
+var vg_4 = "servicesVsPopulation.vg.json";
+vegaEmbed("#population", vg_4).then(function(result) {
+    
+}).catch(console.error);
